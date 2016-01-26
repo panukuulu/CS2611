3b2a9708bdfbbffc38658af6aae0d3c0ae511640
panukuulu <panu.kuuluvainen@gmail.com>
2016-01-26 18:52:20 +0000

    ex1.py test

---
+++ week3/ex1.py
@@ -0,0 +1,8 @@
+__author__ = 'acpm131'
+name = input('Give employee\' name: ')
+hours = input('Enter number of hours worked in a week: ')
+rate = input('Enter hourly pay rate: ')
+tax = input('Enter tax witholding rate (0-1): ')
+tax2 = input('Enter other tax witholding rate (0-1): ')
+
+prin('Emploee name: {:s}\n'.format(name))
